7bed5e4d74834bacbf77fd680e13444e9fd124d7
Ryan <ryanleland@gmail.com>
2010-07-25 16:38:38 -0600

    Added Scene and upgraded plugins

---
+++ libs/Scene.py
@@ -0,0 +1,17 @@
+class Scene():
+	loadScene(self):
+		return True
+	# end loadScene
+		
+	showScene(self):
+		return True
+	# end showScene 
+		
+	hideScene(self):
+		return True
+	# end hideScene
+		
+	unloadScene(self):
+		return True
+	# end unloadScene
+# end Scene
